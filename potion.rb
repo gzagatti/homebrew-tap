93359663ccf187f59498dbf02b2f6348a954a3d3
gzagatti <gzagatti@gmail.com>
2018-02-12 12:27:04 -0500

    potion: install directly from compiled tar.

--- potion.rb
+++ potion.rb
@@ -4,12 +4,11 @@
 class Potion < Formula
  desc "_why the lucky stiff's little language (the official repo... until _why returns)"
  homepage "http://perl11.org/potion"
- url "https://github.com/perl11/potion.git", :tag => "v0.3"
+ url "https://github.com/perl11/potion/releases/download/0.3/potion-0.3.1264-x86_64-apple-darwin14.tar.gz"
+ sha256 "d68f55e301057bcf29f0f56ae314005617ac3bb937c7e706b80165edb8162cc4"
 
   def install
-    system "./configure", "PREFIX=#{prefix}"
-    system "make"
-    system "make install BINDIST='pkg/${PKGBIN}.tar.gz' SUDO="
+    prefix.install Dir["*"]
   end
 
 end
