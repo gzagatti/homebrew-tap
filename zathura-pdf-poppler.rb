6d9b56a1c090f2cef03917ea2c4db172c4a34b14
gzagatti <gzagatti@gmail.com>
2020-02-05 16:15:41 +0800

    updates version and simplifies plugin logic.

--- zathura-pdf-poppler.rb
+++ zathura-pdf-poppler.rb
@@ -1,19 +1,27 @@
 class ZathuraPdfPoppler < Formula
   desc "Adds PDF support to zathura by using the poppler rendering engine."
   homepage "https://pwmt.org/projects/zathura-pdf-poppler/"
-  url "https://pwmt.org/projects/zathura-pdf-poppler/download/zathura-pdf-poppler-0.2.9.tar.xz"
-  sha256 "be0806116ea6e6c95495f5e244e0d70d2551a9058e5574b47d3e5a09bc6592dc"
+  url "https://pwmt.org/projects/zathura-pdf-poppler/download/zathura-pdf-poppler-0.3.0.tar.xz"
+  sha256 "d5cc3a7dae49471b85b503bbb9049c6f8d10903f4a611d721a2e0fefe726d4ed"
 
   depends_on "zathura"
   depends_on "poppler"
 
   def install
+    inreplace "meson.build", "zathura.get_pkgconfig_variable('plugindir')", "prefix"
     args = %W[
       --prefix=#{prefix}
     ]
     system "meson", *args, "build"
     system "ninja -C build"
     system "ninja install -C build"
+
+  end
+
+  def post_install
+    plugin="libpdf-poppler.dylib"
+    plugin_library = HOMEBREW_PREFIX/"lib/zathura/"
+    ln_s prefix/plugin, plugin_library/plugin, :force => true
 
   end
 
