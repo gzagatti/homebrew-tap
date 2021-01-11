acc7a110836f40f6bb84b6cd00302abb7b259406
gzagatti <gzagatti@gmail.com>
2021-01-11 13:05:26 +0800

    upgrades Julia.

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -1,9 +1,9 @@
 class Julia < Formula
   desc "The Julia language"
   homepage "https://julialang.org/"
-  url "https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.2-linux-x86_64.tar.gz"
-  version "1.4.2"
-  sha256 "d77311be23260710e89700d0b1113eecf421d6cf31a9cebad3f6bdd606165c28"
+  url "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz"
+  version "1.5.3"
+  sha256 "f190c938dd6fed97021953240523c9db448ec0a6760b574afd4e9924ab5615f1"
   license "MIT"
 
   def install
