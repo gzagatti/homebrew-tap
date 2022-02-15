9ab8ee91dafb8f5deb13cf252a7c7db5c983e654
gzagatti <gzagatti@users.noreply.github.com>
2022-02-15 11:06:00 +0800

    nnn with custom build.

---
+++ Formula/nnn.rb
@@ -0,0 +1,37 @@
+class Nnn < Formula
+  desc "Tiny, lightning fast, feature-packed file manager"
+  homepage "https://github.com/jarun/nnn"
+  url "https://github.com/jarun/nnn/archive/v4.4.tar.gz"
+  sha256 "e04a3f0f0c2af1e18cb6f005d18267c7703644274d21bb93f03b30e4fd3d1653"
+  license "BSD-2-Clause"
+  head "https://github.com/jarun/nnn.git", branch: "master"
+
+  depends_on "gnu-sed"
+  depends_on "readline"
+
+  uses_from_macos "ncurses"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}", "O_NERD=1"
+    bash_completion.install "misc/auto-completion/bash/nnn-completion.bash"
+    zsh_completion.install "misc/auto-completion/zsh/_nnn"
+    fish_completion.install "misc/auto-completion/fish/nnn.fish"
+  end
+
+  test do
+    # on_linux do
+    #   # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
+    #   # Fixing it involves pty/ruby voodoo, which is not worth spending time on
+    #   return if ENV["HOMEBREW_GITHUB_ACTIONS"]
+    # end
+
+    # Testing this curses app requires a pty
+    require "pty"
+
+    (testpath/"testdir").mkdir
+    PTY.spawn(bin/"nnn", testpath/"testdir") do |r, w, _pid|
+      w.write "q"
+      assert_match "~/testdir", r.read
+    end
+  end
+end
