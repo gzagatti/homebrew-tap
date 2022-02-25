98cf15025ca22e4c032b681a44fa804c0d516954
gzagatti <gzagatti@users.noreply.github.com>
2022-02-25 13:09:42 +0800

    julia: 1.7.2 update

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -1,13 +1,13 @@
 class Julia < Formula
-  desc "The Julia language"
+  desc "Fast, dynamic programming language"
   homepage "https://julialang.org/"
   url "https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.2-linux-x86_64.tar.gz"
-  version "1.7.0"
+  version "1.7.2"
   sha256 "a75244724f3b2de0e7249c861fbf64078257c16fb4203be78f1cf4dd5973ba95"
   license "MIT"
 
   def install
-    Dir["./*"].each { |f| mv f, prefix }
+    prefix.install Dir["./*"]
   end
 
   test do
