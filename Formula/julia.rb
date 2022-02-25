5ef2dc9c7feeda83d038c3ad18395e6131a4a348
gzagatti <gzagatti@users.noreply.github.com>
2022-02-25 13:05:39 +0800

    new version of julia

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -1,9 +1,9 @@
 class Julia < Formula
   desc "The Julia language"
   homepage "https://julialang.org/"
-  url "https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.0-linux-x86_64.tar.gz"
+  url "https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.2-linux-x86_64.tar.gz"
   version "1.7.0"
-  sha256 "7299f3a638aec5e0b9e14eaf0e6221c4fe27189aa0b38ac5a36f03f0dc4c0d40"
+  sha256 "a75244724f3b2de0e7249c861fbf64078257c16fb4203be78f1cf4dd5973ba95"
   license "MIT"
 
   def install
