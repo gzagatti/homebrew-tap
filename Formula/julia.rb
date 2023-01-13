05c8f27eec2926cd067458bbd88b2a858da4759f
gzagatti <gzagatti@users.noreply.github.com>
2023-01-13 12:05:29 +0800

    updates julia.

--- Formula/julia.rb
+++ Formula/julia.rb
@@ -1,9 +1,9 @@
 class Julia < Formula
   desc "Fast, dynamic programming language"
   homepage "https://julialang.org/"
-  url "https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.0-linux-x86_64.tar.gz"
-  version "1.8.0"
-  sha256 "e80d732ccb7f79e000d798cb8b656dc3641ab59516d6e4e52e16765017892a00"
+  url "https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.5-linux-x86_64.tar.gz"
+  version "1.8.5"
+  sha256 "e71a24816e8fe9d5f4807664cbbb42738f5aa9fe05397d35c81d4c5d649b9d05"
   license "MIT"
 
   def install
